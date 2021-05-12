826913c50b922d5615d51508a8190dca0d629974
exponenta.euler <exponenta.euler@gmail.com>
2021-05-12 11:14:54 +0500

    5.Ex.Анаграммы _ Coursera

---
+++ WhiteBeltWeekTwo_Anagramm.cpp
@@ -0,0 +1,46 @@
+﻿// WhiteBeltWeekTwo_Anagramm.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+void PrintMap(const map<int, string>& map) {
+    for (const auto& elem : map) {
+        cout << elem.first << " " << elem.second << endl; 
+    }
+}
+
+map<char, int> BuildCharCounters(const string& s) {
+    map<char, int> result;
+    for (int i = 0; i < s.size(); i++)
+    {
+        ++result[s[i]];
+    }
+    return result;
+}
+
+int main()
+{
+    int n;
+    map<char, int> mapOne;
+    map<char, int> mapTwo;
+    string stringOne;
+    string stringTwo;
+    cout << "enter number of pairs: " << endl;
+    cin >> n;
+    while (n) {
+        cin >> stringOne >> stringTwo;
+        mapOne = BuildCharCounters(stringOne);
+        mapTwo = BuildCharCounters(stringTwo);
+        if (mapOne == mapTwo) {
+            cout << "yes" << endl;
+        }
+        else {
+            cout << "no" << endl;
+        }
+        --n;
+    }
+}
